c2f831309ffb9f4c2ea76d9704496877faa03018
Andrew <andrew@integral.io>
2019-06-27 16:17:03 -0400

    start producing backup

---
+++ copyin.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for dir in config/* config/.*    # list directories in the form "/tmp/dirname/"
+do
+  dir=${dir%*/}      # remove the trailing "/"
+  dir=${dir##*/}     # keep everything after config
+  cmd="cp -rf ~/$dir ./config/"
+  if [ $dir != ".." ] && [ $dir != "." ]; then
+    echo "running: $cmd"    # print everything after the final "/"
+    eval $cmd
+  fi
+done
